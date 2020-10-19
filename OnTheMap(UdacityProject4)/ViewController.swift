ab9ff953c3a9e99f5f0a29e485c576407763d562
Cédric Morier-Roy <cmorierroy@gmail.com>
2020-10-19 13:49:32 -0500

    Initial Commit

---
+++ OnTheMap(UdacityProject4)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnTheMap(UdacityProject4)
+//
+//  Created by Cédric Morier-Roy on 2020-10-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
